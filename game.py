a20545109dfbe2ae1c6cab5ef61de480a1d1473a
jibranabsarulislam <jga26@njit.edu>
2021-07-09 00:26:57 -0400

    fix ghosts text alignment

--- game.py
+++ game.py
@@ -2,15 +2,23 @@ import pygame
 import time
 from copy import copy, deepcopy
 import random
+
+
+# TheForage Module 1
+# Jibran Absarulislam and Nixon Puertollano
+
+
 # completed:
 # Vax-Man can kill a ghost if he comes into contact with it (vaccinates it).
 # Contact with a ghost does not kill Vax-Man.
 # The goal of the game is to collect all the dots before the number of ghosts grows to 32 times the original number.
 #   original number = 4; goal is while num_ghosts < 128
 # Each ghost that has not yet been hit multiplies itself every 30 seconds (the infection grows).
-
-# TODO:
 # Duplicated ghosts have to be able to move
+
+#TODO (later):
+# fix overlapping
+
 
 
 black = (0, 0, 0)
@@ -590,11 +598,11 @@ def startGame():
         screen.blit(text, [10, 10])
 
         text = font.render("Ghosts: "+str(len(monsta_list)), True, red)
-        screen.blit(text, [0, 35])
+        screen.blit(text, [10, 35])
 
         t1 = time.time()  # https://stackoverflow.com/questions/20023709/resetting-pygames-timer
         dt = t1 - t0
-        dupe_time = 3
+        dupe_time = 2
         if dt >= dupe_time:
             print(dupe_time, "seconds reached")
             t0 = t1  # when this is called, timer goes back to "0"
